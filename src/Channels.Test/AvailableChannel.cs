67e498f3198a21d64615cf7a0bd5ba176ac42285
Milad Ashrafi <Mi.Ashrafi@Alibaba.ir>
2022-06-19 18:34:03 +0430

    init

---
+++ src/Channels.Test/AvailableChannel.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace Channels.Test
+{
+    public interface IAvailableChannel
+    {
+        public Task Write(string value);
+        public void BeginRead();
+    }
+
+    public sealed class AvailableChannel : IAvailableChannel
+    {
+        private readonly Channel<string> _channel = Channel.CreateUnbounded<string>();
+        public void BeginRead()
+        {
+            _ = Task.Factory.StartNew(async () =>
+            {
+                while (await _channel.Reader.WaitToReadAsync())
+                {
+                    if (_channel.Reader.TryRead(out var msg))
+                        Console.WriteLine(msg);
+
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                }
+            });
+        }
+
+        public async Task Write(string value)
+        {
+            await _channel.Writer.WriteAsync(value);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+
+    }
+}
---
+++ src/Channels.Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using Channels.Test;
+
+var channel = new AvailableChannel();
+
+channel.BeginRead();
+var index = 0;
+
+foreach (var item in new[] {"A", "B", "C"})
+{
+    for (int i = 0; i < 10; i++)
+    {
+        index++;
+        await channel.Write($"{index}-{item}-{i+1}");
+    }
+}
+
+Console.WriteLine("Complete!");
+
+
+Console.ReadLine();
